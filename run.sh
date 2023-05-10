6aa32aff9d81659eaf44e6d47028ee15d16c1a3e
Phillip Conrad <pconrad@cs.ucsb.edu>
2023-05-10 08:49:32 -0700

    pc - add step to rebuild cmake environment

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# initialize cmake environment
+# this avoids errors that build/release does not exist
+# and errors that the cache cannot be found
+# https://stackoverflow.com/a/74338548/6454116
+
+cmake -S . -B ./build/release 
+
 (
   # utilizing cmake's parallel build options
   # recommended: -j <number of processor cores + 1>
